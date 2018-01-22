6d63901d2c5d2f38e1380a8406f61a4a1ae2ba37
Figueroapenajuan <35306822+Figueroapenajuan@users.noreply.github.com>
2018-01-22 11:47:16 -0600

    codigo grafica hermanos y primos

---
+++ g22.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+plt.plot([4,4,5,5,5,6,7,7,7,9,9,9,9,9,9,9,9,9,9])
+
+plt.ylabel('primos y hermanos por ano  de vida')
+plt.xlabel('anos de vida')
+plt.title('Juan Figueroa ')
+
+
+plt.savefig('temp.png')
+
+plt.show()
